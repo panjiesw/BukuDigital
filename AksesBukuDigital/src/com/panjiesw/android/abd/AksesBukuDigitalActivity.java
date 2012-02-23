9e44f1cbdd5bfae54fe19a790ab72438345b444e
Panjie Setiawan Wicaksono <panjie@panjiesw.com>
2012-02-24 01:05:54 +0700

    modify AksesBukuDigitalActivity, add Assets monocle scripts

--- AksesBukuDigital/src/com/panjiesw/android/abd/AksesBukuDigitalActivity.java
+++ AksesBukuDigital/src/com/panjiesw/android/abd/AksesBukuDigitalActivity.java
@@ -4,6 +4,11 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class AksesBukuDigitalActivity extends Activity {
+	
+	private static final String[] titles = {"Plain Tales from the Hills","Washington Square"};
+	private static final String[] authors = {"Rudyard Kipling","Henry James"};
+	private static final String[] path = {"books/","books/"};
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
